004a3919eb4a81b5e0e9487d3c74aa17c00355dd
Esteban Salazar <96194100+EstebanSalazar-27@users.noreply.github.com>
2024-02-16 16:17:13 -0300

    changes

--- main.go
+++ main.go
@@ -9,38 +9,52 @@ func getUserBalance() float64 {
 	const balance float64 = 1000.0
 	return balance
 }
-func choicesControl(choice int, balance float64) {
-	if choice == 1 {
-		fmt.Print("Your current balance is: ", balance)
-	} else if choice == 2 {
-		var depositAmount float64
-		fmt.Print("Deposit amount: ")
-		fmt.Scan(&depositAmount)
-		balance = balance + depositAmount
-		fmt.Print("Your Current Balance: ", balance)
-	} else if choice == 3 {
-		var witdrawlAmount float64
-		fmt.Print("Witdrawl amount: ")
-		fmt.Scan(&witdrawlAmount)
-		balance = balance - witdrawlAmount
-		print("Witdrawl was sucessfully ")
-		fmt.Print("Your Current Balance: ", balance)
-	} else {
-		print("Sesion Terminada!")
+func choicesControl(balance float64) {
+	var choice int
+	print("Welcome to Go Bank Application")
+	for {
+		print("What would you like to do ?")
+		print("1. Check balance")
+		print("2. Deposit money")
+		print("3. Witdrawl money")
+		print("4. Exit")
+		fmt.Print("Your choice:")
+		fmt.Scan(&choice)
+
+		if choice == 1 {
+			fmt.Println("Your current balance is: ", balance)
+		} else if choice == 2 {
+			var depositAmount float64
+			fmt.Print("Deposit amount: ")
+			fmt.Scan(&depositAmount)
+			if depositAmount <= 0 {
+				print("Deposit must be greater than 0 !")
+				continue
+			}
+			balance = balance + depositAmount
+			fmt.Println("Your Current Balance: ", balance)
+		} else if choice == 3 {
+			var witdrawlAmount float64
+			fmt.Print("Witdrawl amount: ")
+			fmt.Scan(&witdrawlAmount)
+			if witdrawlAmount > balance {
+				print("No enough amount to perform this operation !")
+				continue
+			} else if witdrawlAmount <= 0 {
+				print("Mount must be greater than 0 ")
+				continue
+			}
+			balance = balance - witdrawlAmount
+			print("Witdrawl was sucessfully ")
+			fmt.Print("Your Current Balance: ", balance)
+		} else {
+			print("Sesion Terminada!")
+			break
+		}
 	}
+
 }
 func main() {
 	balance := getUserBalance()
-	print("Welcome to Go Bank Application")
-	print("What would you like to do ?")
-	print("1. Check balance")
-	print("2. Deposit money")
-	print("3. Witdrawl money")
-	print("4. Exit")
-
-	var choice int
-	fmt.Print("Your choice:")
-	fmt.Scan(&choice)
-	choicesControl(choice, balance)
-
+	choicesControl(balance)
 }
